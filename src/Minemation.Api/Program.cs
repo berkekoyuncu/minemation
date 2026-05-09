102e9370723b7ce79e8b990d255b6b29ffde9c65
elifnisaokur <170112633+elifnisaokur@users.noreply.github.com>
2026-05-09 17:52:21 +0300

    API baslatma ve EF Core paket ayarlari eklendi

--- src/Minemation.Api/Program.cs
+++ src/Minemation.Api/Program.cs
@@ -1,41 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Minemation.Api.Middlewares;
+using Minemation.Application.Interfaces;
+using Minemation.Application.Services;
+using Minemation.Infrastructure.Persistence;
+using Minemation.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Veritabanı bağlantısı
+// Şimdilik fake repository kullansak bile DbContext kaydı durabilir.
+// Gerçek repository'ye geçince zaten gerekli olacak.
+builder.Services.AddDbContext<MinemationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Dependency Injection kayıtları
+builder.Services.AddScoped<IPersonelServisi, PersonelServisi>();
+
+// ŞİMDİLİK FAKE REPOSITORY KULLANIYORUZ
+builder.Services.AddScoped<IPersonelRepository, SahtePersonelRepository>();
+
+// GERÇEK VERİTABANINA GEÇİNCE ÜSTTEKİ SAHTE KAYDI KAPATIP BUNU AÇ:
+// builder.Services.AddScoped<IPersonelRepository, PersonelRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+// Ana adrese gidince Swagger'a yönlendirir
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
